ea17244775133c5dc8660eecd88cb6899ad33169
Younes <younes.miftah.sio@gmail.com>
2025-09-03 17:00:20 +0200

    commit:3

--- Repas.java
+++ Repas.java
@@ -5,4 +5,24 @@ public class Repas {
 	private int numero ;
 	private LocalDate date ;
 	
+	public Repas(int numero, LocalDate date) {
+		this.numero = numero ;
+		this.date = date ;
+	}
+	
+	 public int getNumero() {
+	        return numero;
+	    }
+
+	    public void setNumero(int numero) {
+	        this.numero = numero;
+	    }
+
+	    public LocalDate getDate() {
+	        return date;
+	    }
+
+	    public void setDate(LocalDate date) {
+	        this.date = date;
+	    }
 }
